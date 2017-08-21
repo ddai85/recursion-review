44ac6bc7a9ae634c28a5e5b715d9ccc54dac53dd
Hack Reactor Student <student@hackreactor.com>
2017-08-21 12:34:14 -0700

    Write code for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,58 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+// create an array name result
+//if typeof strings want to return string
+// if typeof numbers want to return numbers strings
+//  if typeof boolean want to return boolean to string
+//   if typeof object 
+ //   if Array.isArray(obj)
+ //    if obj.length === 0 return "[]"
+
+  var result = [];
+  
+  if(typeof obj === "string"){
+    return '"' + obj + '"';
+
+  } else if (typeof obj === "number") {
+    return obj + '';   
+
+  } else if(typeof obj === "boolean"){
+    return obj + '';
+    
+  } else if(typeof obj !== "function" && typeof obj !== "undefined"){
+    return '{}';       
+            
+  } else if(typeof obj === "object"){
+
+    if (obj === null){
+      return 'null';
+    }
+    
+    if(Array.isArray(obj)){
+       
+      if(obj.length === 0){
+        return '[]';
+      }
+
+      for(var i = 0; i < obj.length; i++){
+        result.push(stringifyJSON(obj[i]));
+      }
+      return '[' + result.join(',') + ']';
+    } else{
+      
+      if(Object.keys(obj).length === 0){
+        return '{}'; 
+      } else {
+        for(var key in obj){
+          result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+        }
+      }
+      return '{' + result.join(',') + '}';      
+      
+    }
+
+
+  } 
+
 };
