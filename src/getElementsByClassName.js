ea2a786ce95878b04343b55bfa1dbb1933d9092e
Hack Reactor Student <student@hackreactor.com>
2017-08-21 19:40:22 -0700

    Change "Fill_Me_In" field to "true"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,24 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var result = [];
+  // if node has className return node else do nothing
+  var checkClass = function(node) {
+    if (node.classList !== undefined) {
+
+      if (node.classList.contains(className)) {
+        result.push(node);
+      }
+    }
+    
+    for (var i = 0; i < node.childNodes.length; i++) {
+      checkClass(node.childNodes[i]);
+    }
+  };
+
+  checkClass(document.body);
+  // get children of current node
+  // recurse through function on child nodes
+  return result;
+  //document.body, element.childNodes, and element.classList
 };
